13184c2f6d31708cb11da8eb58e138ecb329b106
teresaktang <teresakmtang@gmail.com>
2025-01-12 05:10:19 +0000

    puppet

--- dict.js
+++ dict.js
@@ -1,5 +1,5 @@
 import puppeteer  from "puppeteer";
-
+//web scrape open source dictionary Wiktionary
 
  const getWord = async () => {
     const browser = await puppeteer.launch({
@@ -8,11 +8,12 @@ import puppeteer  from "puppeteer";
     });
     const page = await Browser.newPage();
 
-    await page.gotto("", {
-        waitUntil: "",
+    await page.gotto("https://en.wiktionary.org/w/index.php?title=word&printable=yes", {
+        waitUntil: "domcontentloaded",
     });
+    
+    
  };
 
  getWord();
 
- 
