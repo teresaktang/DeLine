5012d5c2f4ec7a1b5b81fcb433ed2aa7855efe26
teresaktang <teresakmtang@gmail.com>
2025-04-14 22:46:44 +0100

    Definitions

--- deline.js
+++ deline.js
@@ -1,10 +1,58 @@
 const btn = document.getElementById("btn")
-const wordinput = document.getElementById("input")
-const word = deline
+const wordInput = document.getElementById("input")
+const displayOutput = document.getElementById("output")
+const line = document.getElementById("highlight")
+class Define {
+   constructor () {
+    //event listener
+   btn.addEventListener("click", () => {
+        const word = wordInput.value.trim();
+        if (word) {
+            this.getWord(word);
+        } else {
+            displayOutput.textContent = "Enter a word"
+        }
+    });
+}
+async getWord(word) {
+    try {
+        const response = await fetch(
+            `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+        );
+        if (!response.ok) {
+            throw new Error("Sorry, word not found. Please try a different word")
+        }
+        const data = await response.json();
+        this.displayMeaning(data);
+    }   catch (error) {
+        displayOutput.textContent = error.message;
+    }   finally {
+        wordInput.value = "";
+    }
+}
+    displayMeaning(data) {
+        const wordData = data[0];
+        
+        const meanings = wordData.meanings
+            .map((meaning) => {
+                const definitions = meaning.definitions
+                    .map((def) = `<li>${def.definition}</li>`)
+                    .join("");
+                return `<div class="result-item"><strong>${meaning.partOfSpeech}</strong><ul>${definitions}</ul></div>`;
+            })
+            .join("");
+
+        displayOutput.innterHTML = `
+        <h2 class="result-title">${wordData.word}</h2>
+        <div>${meanings}</div>
+        <div class="result-item"><a href="${wordData.sourceUrls[0]}" target="_blank">Source</a></div
+        `;
+    };
+}
+    
+new Define();
 
 //highlight define function
-class deline{
+function highlight(highlight) {
 
 }
-
-
